d568c2a641bdf6e013c0fbfd3cc6fcfb99914d7f
Bugrova Irina <bugrova147@yandex.ru>
2025-02-05 21:22:14 +0300

    added exceptions to classes Product, Category

--- src/category.py
+++ src/category.py
@@ -1,5 +1,5 @@
 from lawngrass import grass1, grass2
-from products import Product, product1, product2, product3
+from products import Product
 from smartphone import smartphone1, smartphone2, smartphone3
 
 
@@ -11,7 +11,7 @@ class Category:
 
     name: str
     description: str
-    __products: list
+    products: list
 
     def __init__(self, name, description, products=None):
         self.name = name
@@ -47,72 +47,97 @@ class Category:
         cls.category_count = 0
         cls.product_count = 0
 
+    def middle_price(self):
+        """Подсчет среднего ценника всех товаров"""
+        try:
+            return sum([product.price for product in self.__products]) / len(
+                self.__products
+            )
+        except ZeroDivisionError:
+            return 0
+
 
 # if __name__ == "__main__":
-category1 = Category(
-    "Смартфоны",
-    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-    [product1, product2, product3],
-)
-
-print(category1.products)
-product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-category1.add_product(product4)
-print(category1.products)
-print(category1.product_count)
+# category1 = Category(
+#     "Смартфоны",
+#     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#     [product1, product2, product3],
+# )
+#
+# print(category1.products)
+# product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+# category1.add_product(product4)
+# print(category1.products)
+# print(category1.product_count)
 
 # product5 = ('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
 # category1.add_product(product5)
 # print(category1.products)
 
-print(str(category1))
+# print(str(category1))
 
 # print(category1.products)
 
-category_smartphones = Category(
-    "Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2]
-)
-category_grass = Category(
-    "Газонная трава", "Различные виды газонной травы", [grass1, grass2]
-)
+# category_smartphones = Category(
+#     "Смартфоны", "Высокотехнологичные смартфоны", [smartphone1, smartphone2]
+# )
+# category_grass = Category(
+#     "Газонная трава", "Различные виды газонной травы", [grass1, grass2]
+# )
+#
+# category_smartphones.add_product(smartphone3)
+#
+# print(category_smartphones.products)
+#
+# print(Category.product_count)
+#
+# try:
+#     category_smartphones.add_product("Not a product")
+# except TypeError:
+#     print("Возникла ошибка TypeError при добавлении не продукта")
+# else:
+#     print("Не возникла ошибка TypeError при добавлении не продукта")
+#
+#
+# category1 = Category(
+#     "Смартфоны",
+#     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+#     [product1, product2, product3],
+# )
+#
+# print(category1.name == "Смартфоны")
+# print(category1.description)
+# print(len(category1.products))
+# print(category1.category_count)
+# print(category1.product_count)
+#
+# product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
+# category2 = Category(
+#     "Телевизоры",
+#     "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
+#     [product4],
+# )
+#
+# print(category2.name)
+# print(category2.description)
+# print(len(category2.products))
+# print(category2.products)
+#
+# print(Category.category_count)
+# print(Category.product_count)
 
-category_smartphones.add_product(smartphone3)
+if __name__ == "__main__":
+    product1 = Product(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+    )
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
 
-print(category_smartphones.products)
+    category1 = Category(
+        "Смартфоны", "Категория смартфонов", [product1, product2, product3]
+    )
 
-print(Category.product_count)
+    print(category1.middle_price())
 
-try:
-    category_smartphones.add_product("Not a product")
-except TypeError:
-    print("Возникла ошибка TypeError при добавлении не продукта")
-else:
-    print("Не возникла ошибка TypeError при добавлении не продукта")
-
-
-category1 = Category(
-    "Смартфоны",
-    "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
-    [product1, product2, product3],
-)
-
-print(category1.name == "Смартфоны")
-print(category1.description)
-print(len(category1.products))
-print(category1.category_count)
-print(category1.product_count)
-
-product4 = Product('55" QLED 4K', "Фоновая подсветка", 123000.0, 7)
-category2 = Category(
-    "Телевизоры",
-    "Современный телевизор, который позволяет наслаждаться просмотром, станет вашим другом и помощником",
-    [product4],
-)
-
-print(category2.name)
-print(category2.description)
-print(len(category2.products))
-print(category2.products)
-
-print(Category.category_count)
-print(Category.product_count)
+    category_empty = Category("Пустая категория", "Категория без продуктов", [])
+    print(category_empty.middle_price())
--- src/products.py
+++ src/products.py
@@ -14,7 +14,10 @@ class Product(BaseProduct, PrintMixin):
         self.name = name
         self.description = description
         self.__price = price
-        self.quantity = quantity
+        if quantity > 0:
+            self.quantity = quantity
+        else:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         super().__init__()
 
     def __str__(self):
@@ -53,26 +56,26 @@ class Product(BaseProduct, PrintMixin):
         self.__price = new_price
 
 
-product1 = Product(
-    "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
-)
-product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
-product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
-
-print(product1.name)
-print(product1.description)
-print(product1.price)
-print(product1.quantity)
-
-print(product2.name)
-print(product2.description)
-print(product2.price)
-print(product2.quantity)
-
-print(product3.name)
-print(product3.description)
-print(product3.price)
-print(product3.quantity)
+# product1 = Product(
+#     "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5
+# )
+# product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+# product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+#
+# print(product1.name)
+# print(product1.description)
+# print(product1.price)
+# print(product1.quantity)
+#
+# print(product2.name)
+# print(product2.description)
+# print(product2.price)
+# print(product2.quantity)
+#
+# print(product3.name)
+# print(product3.description)
+# print(product3.price)
+# print(product3.quantity)
 
 # new_product = Product.new_product(
 #     {
@@ -102,3 +105,14 @@ print(product3.quantity)
 # print(product1 + product2)
 # print(product1 + product3)
 # print(product2 + product3)
+
+try:
+    product_invalid = Product("Бракованный товар", "Неверное количество", 1000.0, 0)
+except ValueError as e:
+    print(
+        "Возникла ошибка ValueError прерывающая работу программы при попытке добавить продукт с нулевым количеством"
+    )
+else:
+    print(
+        "Не возникла ошибка ValueError при попытке добавить продукт с нулевым количеством"
+    )
